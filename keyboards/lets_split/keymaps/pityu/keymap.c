fd60e0bf0704a4351317d80d763776b0addcab25
Istvan Bartha <bartha.pityu@gmail.com>
2018-06-21 20:27:55 +0200

    - and = on front layer

--- keyboards/lets_split/keymaps/pityu/keymap.c
+++ keyboards/lets_split/keymaps/pityu/keymap.c
@@ -30,20 +30,20 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Qwerty
  * ,-----------------------------------------------------------------------------------.
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+ * | Esc  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
+ * | Tab  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shift |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |ARROW | Ctrl | Alt  | GUI  |Space | Lower|Enter |Raise |Ctrl-B|      |      |      |
+ * |ARROW | Ctrl | Alt  | GUI  |Space | Lower|Enter |Raise |Ctrl-B|  -_  | =+   |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_ortho_4x12(
-   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+   KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
+   KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
    KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC , \
-   ARROW,  KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,   LOWER,  KC_ENT ,  RAISE,   CTRLB, _______, _______,   _______  
+   ARROW,  KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,   LOWER,  KC_ENT ,  RAISE,   CTRLB, KC_MINUS, KC_EQUAL,   _______  
 ),
 
 /* Colemak
